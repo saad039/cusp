48daa2cf2eaa7cbb8e11cb2485072358cce8b465
Muhammad Saad <msaadahmed039@gmail.com>
2020-02-24 18:43:41 +0500

    added patterns for logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,28 @@
-#include"cusppch.h"
+#include "cusppch.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
+#define COLOR_LOGGING_PATTERN "%^%v%$"
+#define WHITE_LOGGING_PATTERN "%^%v"
 
-
-
-
-int main() 
+int main()
 {
-    spdlog::info("Welcome to spdlog!");
-    spdlog::error("Some error message with arg: {}", 1);
+    auto console = spdlog::stdout_color_mt("console");
+    console->set_pattern(COLOR_LOGGING_PATTERN);
+
+    console->info("Welcome to spdlog!");
+    console->warn("please be careful");
+    console->error("Some error message with arg{}..", 1);
+    console->info("Welcome to spdlog!");
     
-    spdlog::warn("Easy padding in numbers like {:08d}", 12);
-    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    spdlog::info("Support for floats {:03.2f}", 1.23456);
-    spdlog::info("Positional args are {1} {0}..", "too", "supported");
-    spdlog::info("{:<30}", "left aligned");
-    
-    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-    spdlog::debug("This messages should be displayed..");    
-    
-    // change log pattern
-    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-    
-    // Compile time log levels
-    // define SPDLOG_ACTIVE_LEVEL to desired level
-    SPDLOG_TRACE("Some trace message with param {}", 42);
-    SPDLOG_DEBUG("Some debugging message");
-    
-    
+    console->set_pattern(WHITE_LOGGING_PATTERN);
+
+    console->error("Some error message with arg{}..", 1);
+    console->info("Welcome to spdlog!");
+    console->error("Some error messages with arg{}..", 1);
+
     // Set the default logger to file logger
     // auto file_logger = spdlog::basic_logger_mt("basic_logger", "logs/basic.txt");
-    // spdlog::set_default_logger(file_logger);            
+    // spdlog::set_default_logger(file_logger);
+
+    return 0;
 }
